0d2300193e1d9d1e90ec2cf4ff6fb863e03fdcff
shivu joshi <shivranjanjoshi@gmail.com>
2024-10-17 22:29:32 +0530

    make changes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express=require("express");
+const path=require("path")
 const urlroute=require("./routes/url");
 const {connecttomongodb}=require("./connect")
 const app=express();
 const URL=require('./models/url')
 const PORT=8007;
 
+
 connecttomongodb("mongodb://localhost:27017/short-url")
 .then(()=>console.log("mongodb connected"));
 
+
+app.set("View engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
-app.get('/:shortId', async (req,res)=>{
+app.get('/url/:shortId', async (req,res)=>{
     const shortId=req.params.shortId;
     const entry=await URL.findOneAndUpdate({
         shortId
@@ -22,6 +28,11 @@ app.get('/:shortId', async (req,res)=>{
     res.redirect(entry.redirectURL);
 
 })
+
+app.get('/test', async(req,res)=>{
+    const allurls=await URL.find({});
+    return res.render("home");
+});
 
 app.use("/url", urlroute)
 app.listen(PORT, ()=>console.log(`server started at PORT:${PORT}`));
